071c337eb4f2d78e0ea9b12c10d0c8a20f32e05b
Ka <kawong2020@u.northwestern.edu>
2017-09-09 17:22:15 -0500

    Updated and added Labels and Buttons for UI

---
+++ SuperAdventure Project/SuperAdventure.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace User_Interface_Project
+{
+    public partial class SuperAdventure : Form
+    {
+        public SuperAdventure()
+        {
+            InitializeComponent();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            lblGold.Text = "123";
+        }
+    }
+}
