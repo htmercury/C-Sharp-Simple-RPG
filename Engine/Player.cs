c6c8ba1752f7df75f916d9243caef386742c7517
Ka <kawong2020@u.northwestern.edu>
2017-09-10 15:47:49 -0500

    Established base classes for inheritance

--- Engine/Player.cs
+++ Engine/Player.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Engine
 {
-    public class Player
+    public class Player: LivingCreature
     {
-        public int CurrentHitPoints { get; set; }
-        public int MaximumHitPoints { get; set; }
         public int Gold { get; set; }
         public int ExperiencePoints { get; set; }
         public int Level { get; set; }
